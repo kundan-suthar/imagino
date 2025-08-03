2eaf0be992ca56f99edc62dbb229ff5d997e98ae
Kundan <kundanss22@gmail.com>
2025-08-03 11:54:33 +0530

    added all required components and header started

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap'>
+            Header
+        </header>
+    )
+}
+
+export default Header
