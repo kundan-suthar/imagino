c0bf377d9dd47c1a985934bc21bd9f081277e4c2
Kundan <kundanss22@gmail.com>
2025-08-08 19:56:27 +0530

    updated button primary and glass classes

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -20,8 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        primary: "backdrop-blur-lg bg-gradient-to-r from-blue-500 to-purple-600 text-white border-transparent hover:shadow-2xl hover:shadow-blue-500/25 hover:scale-105 hover:transform",
-        glass: "backdrop-blur-lg bg-white/10  text-white border-white/20  hover:bg-white/20 hover:scale-105 hover:transform"
+        primary: "backdrop-blur-lg bg-gradient-to-r from-blue-500 to-purple-600 text-white border-transparent hover:shadow-2xl hover:shadow-blue-500/25 hover:scale-105 hover:transform hover:cursor-pointer",
+        glass: "backdrop-blur-lg bg-white/10  text-white border-white/20  hover:bg-white/20 hover:scale-105 hover:transform hover:cursor-pointer"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
